91691a3dc736021a36bbbdd551c141fd73d636e8
mustafa <debf2@ymail.com>
2023-03-10 14:27:47 +0300

    add install to readme

--- app/src/main/java/com/mzgs/adshelper/MainActivity.kt
+++ app/src/main/java/com/mzgs/adshelper/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.mzgs.adshelper
+package com.mzgs.adshelpertest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mzgs.adshelper.Ads
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Ads.Print("test")
+
     }
 }
