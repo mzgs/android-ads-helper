3d16faf3333b05af3c8577baa2f0bc648853281e
mustafa <debf2@ymail.com>
2023-03-10 14:16:13 +0300

    first commit

---
+++ AdsHelperLib/src/main/java/com/mzgs/adshelper/Ads.kt
@@ -0,0 +1,17 @@
+package com.mzgs.adshelper
+
+import android.util.Log
+
+class Ads {
+
+    companion object{
+
+        fun  Print  (msg:Any ) {
+            Log.d("mzgs_ads", msg.toString());
+        }
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/mzgs/adshelper/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mzgs.adshelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
